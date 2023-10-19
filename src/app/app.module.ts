478ee1c3b8f7e5ad4cbe9a6de0d484b1ef9a7640
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-10-19 14:30:37 -0300

    angular dio project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardsComponent } from './components/small-cards/small-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleComponent,
+    HomeComponent,
+    BigCardComponent,
+    SmallCardsComponent
   ],
   imports: [
     BrowserModule,
