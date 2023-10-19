1339d48ad8801efbc3fa49f41c18b57ac67d2bd1
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-10-19 15:38:36 -0300

    responsiveness fixed

--- src/app/components/small-cards/small-cards.component.ts
+++ src/app/components/small-cards/small-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-cards',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-cards.component.css']
 })
 export class SmallCardsComponent {
-
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
 }
